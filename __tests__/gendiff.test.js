eb02d7f6d73c281b999c5118c4a4b44ff66d21db
Kursakov Alexnder <sashavalezhnik@yandex.ru>
2023-10-07 16:24:54 +0300

    add

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import { test, expect } from '@jest/globals';
 
-let result;
 test('flatTest', () => {
   const res = fs.readFileSync('../__fixtures__/flatResult.txt', 'utf-8');
   expect(res).toEqual(res);
