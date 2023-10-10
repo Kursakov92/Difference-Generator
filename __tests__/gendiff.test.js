e30ac168f4921b9dfa2cbd3cfc97006f3c0a38cc
Kursakov Alexnder <sashavalezhnik@yandex.ru>
2023-10-10 13:56:13 +0300

    add YAML test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,11 +7,20 @@ import gendiff from '../src';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('flatTest', () => {
+test('flatTestJSON', () => {
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
   const res = fs.readFileSync(getFixturePath('flatResult.txt'), 'utf-8');
   const fl1 = getFixturePath('file1.json');
   const fl2 = getFixturePath('file2.json');
+  const result = gendiff(fl1, fl2);
+  expect(res).toEqual(result);
+});
+
+test('flatTestYAML', () => {
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const res = fs.readFileSync(getFixturePath('flatResult.txt'), 'utf-8');
+  const fl1 = getFixturePath('file1.yaml');
+  const fl2 = getFixturePath('file2.yaml');
   const result = gendiff(fl1, fl2);
   expect(res).toEqual(result);
 });
